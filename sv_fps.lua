b105df5cd21e3504674fef9dfda557003af379cd
DonSamuele <66538187+DonSamuelee@users.noreply.github.com>
2022-01-10 17:23:25 +0100

    Delete sv_fps.lua

--- sv_fps.lua
+++
@@ -1,9 +0,0 @@
-ESX	= nil
-TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
-
-RegisterCommand('fps', function(source)
-    local _source = source
-    local xPlayer = ESX.GetPlayerFromId(_source)
-    TriggerClientEvent('fps:menu', _source)
-end)
-
